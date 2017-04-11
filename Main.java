6f7bce835986d6c2c2d02e61b70f678f5106b200
jakjack <ppujadas14@gmail.com>
2017-04-11 12:05:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
-
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.text.DecimalFormat;
 import javax.swing.JFrame;
 
-import java.awt.*;
 
-public class Main extends JFrame {
+
+public class Main{
 	
-	
-
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4648172894076113183L;
+	JFrame window = new JFrame();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public Main()
+	{
+		
+		window.setTitle("k");
+		window.setSize(2, 3); 
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
 	}
 
 }
